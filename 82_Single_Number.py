92b5a7d399e285350a6c6d7517f55c5655ea009d
Jack Li <aaalizhigang@163.com>
2017-02-16 22:54:09 +0800

    update

---
+++ 82_Single_Number.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+'''
+@Copyright:LintCode
+@Author:   aaajack
+@Problem:  http://www.lintcode.com/problem/single-number
+@Language: Python
+@Datetime: 17-02-16 13:57
+'''
+
+class Solution:
+    """
+    @param A : an integer array
+    @return : a integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        if not A:
+            return 0
+        else:
+            B = []
+            for i in range(len(A)):
+                if A[i] in B:
+                    B.remove(A[i])
+                else:
+                    B.append(A[i])
+        return B[0]
+            
+        
